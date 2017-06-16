693dc1c090c85af803d6a2c720a47172feceb80a
Hank35 <heincrebolder@gmail.com>
2017-06-16 10:34:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var config      = require('./config.json');
+var http        = require('http');
+var express     = require('express');
+
+// Create the application
+var app = express();
+
+app.set('PORT', config.webPort);
+
+app.all('*', function(req, res, next){
+    console.log( req.method + " " + req.url);
+    next();
+});
+
+app.get('/api/v1/hello', function(req, res, next) {
+    res.contentType('application/json');
+    res.json( {"msg":"Hello to you all !! Have a nice NodeJS day."} );
+});
+
+
+// Middleware statische bestanden (HTML, CSS, images)
+app.use(express.static(__dirname + '/public'));
+
+// Routing with versions
+app.use('/apiv1', require('./routes/routes_apiv1'));
+
+
+// Start the server
+var port = process.env.PORT || app.get('PORT');
+
+app.listen(port, function() {
+    console.log('The magic happens at http://localhost:'+ port);
+});
+
+module.exports = app;
+
